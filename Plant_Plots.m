2d88f56c3c5dd14acfeeb042e9eacd8a349902b3
Josh_ <Josh_@Josh-PC>
2021-02-02 01:54:21 +0000

    Added draft of linearised plant model and small script for plotting. Notes on how i derived the model are on Teams.

---
+++ Plant_Plots.m
@@ -0,0 +1,22 @@
+%plots are just for yaw and heading.
+
+subplot(2,1,1)
+hold on
+title('Heading, Yaw, Yaw Rate')
+plot(heading)
+plot(yaw_rate)
+plot(yaw_pos)
+legend('Heading','Yaw Rate','Yaw')
+xlabel('Time (s)')
+ylabel('rads')
+
+grid on
+hold off
+
+subplot(2,1,2)
+hold on
+title('Altitude')
+plot(z_pos);
+xlabel('Time (s)')
+ylabel('Meters')
+grid on
