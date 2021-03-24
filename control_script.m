daef12c8862c3ec3a3c402e39a7ec2d598e94b87
Maria Dias Neves <2312034D@student.gla.ac.uk>
2021-03-24 23:53:43 +0000

    tried to do the outer loop control and it did not work.

--- control_script.m
+++ control_script.m
@@ -10,38 +10,38 @@
 % % Then use CSD:
 % controlSystemDesigner(sys)
 
-% For Vze (Altitude Rate Controller):
-sys1 = tf(LinearAnalysisToolProject.LocalVariables(1).Value);
-% For minimal realization and pole-zero cancellation:
-vz_sys = minreal(sys1, 0.001);
-% Then put TF in TF block and used the PID block.
-
-% For p (Roll Rate Controller):
-sys2 = tf(LinearAnalysisToolProject.LocalVariables(7).Value);
-% For minimal realization and pole-zero cancellation:
-p_sys = minreal(sys2, 0.001);
-% Then put TF in TF block and used the PID block.
-
-% For q (Pitch Rate Controller):
-sys3 = tf(LinearAnalysisToolProject.LocalVariables(6).Value);
-% For minimal realization and pole-zero cancellation:
-q_sys = minreal(sys3, 0.001);
-% Then put TF in TF block and used the PID block.
-
-% For r (yaw Rate Controller):
-sys4 = tf(LinearAnalysisToolProject.LocalVariables(8).Value);
-% For minimal realization and pole-zero cancellation:
-r_sys = minreal(sys4, 0.001);
-% Then put TF in TF block and used the PID block.
+% % For Vze (Altitude Rate Controller):
+% sys1 = tf(LinearAnalysisToolProject.LocalVariables(1).Value);
+% % For minimal realization and pole-zero cancellation:
+% vz_sys = minreal(sys1, 0.001);
+% % Then put TF in TF block and used the PID block.
+% 
+% % For p (Roll Rate Controller):
+% sys2 = tf(LinearAnalysisToolProject.LocalVariables(7).Value);
+% % For minimal realization and pole-zero cancellation:
+% p_sys = minreal(sys2, 0.001);
+% % Then put TF in TF block and used the PID block.
+% 
+% % For q (Pitch Rate Controller):
+% sys3 = tf(LinearAnalysisToolProject.LocalVariables(6).Value);
+% % For minimal realization and pole-zero cancellation:
+% q_sys = minreal(sys3, 0.001);
+% % Then put TF in TF block and used the PID block.
+% 
+% % For r (yaw Rate Controller):
+% sys4 = tf(LinearAnalysisToolProject.LocalVariables(8).Value);
+% % For minimal realization and pole-zero cancellation:
+% r_sys = minreal(sys4, 0.001);
+% % Then put TF in TF block and used the PID block.
 
 % For x position(X Controller):
-sys5 = tf(prescale(LinearAnalysisToolProject.LocalVariables(2).Value));
+sys5 = tf(prescale(LinearAnalysisToolProject.LocalVariables(1).Value));
 % For minimal realization and pole-zero cancellation:
 x_sys = minreal(sys5, 0.001);
 % Then put TF in TF block and used the PID block.
 
 % For y position(Y Controller):
-dsys6 = tf(prescale(LinearAnalysisToolProject.LocalVariables(3).Value));
+sys6 = tf(prescale(LinearAnalysisToolProject.LocalVariables(3).Value));
 % For minimal realization and pole-zero cancellation:
 y_sys = minreal(sys6, 0.001);
 % Then put TF in TF block and used the PID block.
@@ -50,6 +50,12 @@ y_sys = minreal(sys6, 0.001);
 sys7 = tf(LinearAnalysisToolProject.LocalVariables(4).Value);
 % For minimal realization and pole-zero cancellation:
 z_sys = minreal(sys7, 0.001);
+% Then put TF in TF block and used the PID block.
+
+% For yaw angle (Yaw Controller):
+sys8 = tf(LinearAnalysisToolProject.LocalVariables(2).Value);
+% For minimal realization and pole-zero cancellation:
+yaw_sys = minreal(sys8, 0.001);
 % Then put TF in TF block and used the PID block.
 
 
